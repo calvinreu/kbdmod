474bb7bd2a781ec07c8fe5c5e75b1f841a1fb5f2
calvinreu <j.calvin.reu@protonmail.com>
2022-06-16 14:36:30 +0200

    config structs to load config

--- src/config.h
+++ src/config.h
@@ -1,27 +1,41 @@
 #pragma once
+#include <vector>
 
-struct OutputConfig {
-    bool IgnoreConsumption;//do not consume this key
-    bool WaitForRelease;//wait for release to output hold do not
-
+enum HoldStart {
+    after_release = 0,
+    after_press,
+    after_timeout,
+    before_consume,//not possible with Consumption[0] true
 };
 
-struct HoldOutput {
-    int  HoldTimer;//keep hold output active until timer runs out
+enum OutputMode {
+    osm,
+    sequence,
 };
 
-struct TapOutput
-{
-    /* data */
+struct outputSeq {
+    __S16_TYPE code;
+    outputSeq* next;
 };
 
+struct OutputConfig{
+    OutputMode mode;
+    outputSeq output;
+};
+
+struct KeyConfig {
+    int Consumption;//32 bit [1-number of key actions currently hold and tap] trigger consumption in other keys for tap and hold action [0]be able to get consumed
+    HoldStart holdStart;
+    outputSeq tap;
+    outputSeq hold;
+};
 
 struct mapping
 {
     const KeyState  state;
-    const HoldOutput holdOutput;
-    const TapOutput  tapOutpot ;
-    void(*actionHold)(const HoldOutput *output);
-    void(*actionTap )(const TapOutput  *output);
+    const outputSeq holdOutput;
+    const outputSeq  tapOutpot ;
+    void(*actionHold)(const outputSeq *output);
+    void(*actionTap )(const outputSeq  *output);
     //key index is the index in array
 };
