d0eaa0b953d57ccfdec1c99d817ae8d049b85c01
calvinreu <j.calvin.reu@protonmail.com>
2022-06-17 19:51:01 +0200

    remove io handling

--- src/input_loop.h
+++ src/input_loop.h
@@ -1,17 +1,14 @@
 #pragma once
-/* https://www.kernel.org/doc/html/latest/input/event-codes.html */
-#define INPUT_VAL_PRESS 1
-#define INPUT_VAL_RELEASE 0
-#define INPUT_VAL_REPEAT 2//ignore
 #include "timer.h"
 #include "mapping.h"
 #include "consumption.h"
 #include <linux/input.h>
 #include <bitset>
+#include "io.h"
 
 using std::bitset;
 
 extern ExecutionQueue EventQueue;
+extern IOTYPE IO;
 
 extern inline void input_loop();
-extern void write_event(input_event *event);
