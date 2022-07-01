c3b2a45028aafe44dd673756ae0878316652d14c
calvinreu <j.calvin.reu@protonmail.com>
2022-07-01 13:47:04 +0200

    .

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -5,14 +5,37 @@ mapping keyMapBase[KEY_OPTION_MAX - KEY_OPTION_MIN];
 extern ExecutionQueue EventQueue;
 extern IOTYPE IO;
 
-//output press action
-inline void mapping::output_press(const outputSeq &seq) {
-    IO.write_event(seq, INPUT_VAL_PRESS);
-}
-
-//output release action
-inline void mapping::output_release(const outputSeq &seq) {
-    IO.write_event(seq, INPUT_VAL_RELEASE);
+//write output event
+template<output_type type>
+inline void mapping::write_output() {
+    if constexpr (type == tapT)
+        //check if tap is osm
+        if (key & ON_TAP_OSM_MASK)
+            IO.add_osm(tap);
+        else
+            IO.write_event(tap);
+    else if constexpr (type == doubletapT)
+        //check if doubletap is osm
+        if (key & ON_DOUBLETAP_OSM_MASK)
+            IO.add_osm(doubletap);
+        else
+            IO.write_event(doubletap);
+    else if constexpr (type == holdT)
+        //check if hold is osm
+        if (key & ON_HOLD_OSM_MASK)
+            IO.add_osm(hold);
+        else {
+            IO.write_event_press(hold);
+            key |= (HOLD_OUTPUT_PRESSED_MASK + OUTPUT_PRESSED_MASK);
+        }
+    else if constexpr (type == tapholdT)
+        //check if taphold is osm
+        if (key & ON_TAPHOLD_OSM_MASK)
+            IO.add_osm(taphold);
+        else {
+            IO.write_event_press(taphold);
+            key |= (TAPHOLD_OUTPUT_PRESSED_MASK + OUTPUT_PRESSED_MASK);
+        }
 }
 
 inline void mapping::consume() {
@@ -29,6 +52,9 @@ if this is the 3rd release in one event timer
 which shouldnt be possible with a reasonable hold timeout
 the double tap sytem will get fucked anyway
     */
+   //check if output is pressed or if it is passtrhough key
+    if (key < ON_TAP_OSM || key & OUTPUT_PRESSED_MASK)
+        EventQueue.AddEvent(this);
 }
 
 inline void mapping::press() {
@@ -48,9 +74,23 @@ inline void mapping::press() {
 }
 
 inline void mapping::output_event() {
+
+    //check if output is pressed
+    if (key & OUTPUT_PRESSED_MASK) {
+        if (key & TAPHOLD_OUTPUT_PRESSED_MASK) {
+            output_release(taphold);
+            //set output pressed and taphold pressed false
+            key &= ~(OUTPUT_PRESSED_MASK + TAPHOLD_OUTPUT_PRESSED_MASK);
+        }else{
+            output_release(hold);
+            //set output pressed and hold pressed false
+            key &= ~(OUTPUT_PRESSED_MASK + HOLD_OUTPUT_PRESSED_MASK);
+        }
+    }
+
     //check if key should be passed to output
-    if (key < HOLD_ENABLED_MASK) {
-        IO.write_event(tap);
+    if (key < ON_TAP_OSM_MASK) {
+        IO.write_event_press(hold);
         return;
     }
 
@@ -61,23 +101,27 @@ inline void mapping::output_event() {
     switch (key & KEY_STATE) {
     //key not pressed anymore block
     case STATE_DOUBLE_TAP_MASK + STATE_PRESSANDRELEASE_MASK:
-        IO.write_event(tap);//intentionally fallthrough
+        EventQueue.RemoveEvent(this);//fallthrough
+    case STATE_DOUBLE_TAP_MASK + 
+    STATE_PRESSANDRELEASE_MASK + STATE_TAPHOLD_MASK:
+        write_output<tapT>();//intentionally fallthrough
         //set pressandrelease false
         key &= ~STATE_PRESSANDRELEASE_MASK;
-        EventQueue.RemoveEvent(this);
     case STATE_DOUBLE_TAP_MASK:
-        //set double tap false
-        key &= ~STATE_DOUBLE_TAP_MASK;
         EventQueue.RemoveEvent(this);
+        //fallthrough
+    case STATE_DOUBLE_TAP_MASK + STATE_TAPHOLD_MASK:
+        //set double tap false
+        key &= ~(STATE_DOUBLE_TAP_MASK + STATE_TAPHOLD_MASK);
 
         if(doubletap.is_empty())
-            IO.write_event(tap);//intentionally fallthrough
+            write_output<tapT>();//intentionally fallthrough
         else {
-            IO.write_event(doubletap);
+            write_output<doubletapT>();
             return;
         }
     case STATE_PRESSANDRELEASE_MASK:
-        IO.write_event(tap);
+        write_output<tapT>();
         //set pressandrelease false
         key &= ~STATE_PRESSANDRELEASE_MASK;
         return;
@@ -85,10 +129,11 @@ inline void mapping::output_event() {
     case STATE_PRESSED_MASK:
         //set pressandrelease true
         key |= STATE_PRESSANDRELEASE_MASK;
-        IO.write_event(hold);
-        break;
+        write_output<holdT>();
+        return;
     //taphold block
-    case STATE_DOUBLE_TAP_MASK + STATE_PRESSED_MASK + STATE_PRESSANDRELEASE_MASK:
+    case STATE_DOUBLE_TAP_MASK + 
+    STATE_PRESSED_MASK + STATE_PRESSANDRELEASE_MASK:
         //possibilities are:
         //1. tap taphold
         //2. doubletap hold
@@ -103,14 +148,27 @@ inline void mapping::output_event() {
         key &= ~(STATE_DOUBLE_TAP_MASK + STATE_TAPHOLD_MASK);
         //check if taphold is enabled
         if (key & TAPHOLD_ENABLED_MASK) {
-            IO.write_event(taphold);
-            return;
-        }else if (key & DOUBLE_TAP_ENABLED_MASK) {
-            IO.write_event(doubletap);
-            return;
-        {
-            
+            write_output<tapholdT>();
+        }else {
+            write_output<tapT>();
+            write_output<holdT>();
         }
-        
+        return;
+    case KEY_STATE://everything active
+        //set everything but pressed false
+        key &= ~(STATE_PRESSANDRELEASE_MASK + 
+        STATE_TAPHOLD_MASK + STATE_DOUBLE_TAP_MASK);
+        //check if taphold is enabled
+        if (key & TAPHOLD_ENABLED_MASK) {
+            write_output<tapT>();
+            write_output<tapholdT>();
+        //check if double tap is enabled
+        }else if (key & DOUBLE_TAP_ENABLED_MASK) {
+            write_output<doubletapT>();
+            write_output<holdT>();
+        }else {
+            write_output<tapT>();
+            write_output<holdT>();
+        }
     }
 }
--- src/mapping.h
+++ src/mapping.h
@@ -16,20 +16,24 @@
 #define STATE_DOUBLE_TAP 1
 #define STATE_TAPHOLD 2
 #define STATE_PRESSANDRELEASE 3
-#define HOLD_ENABLED 12//has to stay true to indicate that the key has actions
+#define OUTPUT_PRESSED 4
+#define HOLD_ENABLED 12
 #define DOUBLE_TAP_ENABLED 13//can be set to false since vars will be set null
 #define TAPHOLD_ENABLED 14   //can be set to false since vars will be set null
-#define ON_TAP_OSM 8
+#define ON_TAP_OSM 11//has to be smaller than features and bigger than any osm
 #define ON_HOLD_OSM 9
 #define ON_DOUBLETAP_OSM 10
-#define ON_TAPHOLD_OSM 11
+#define ON_TAPHOLD_OSM 8
 #define UNCONSUMABLE 15
+#define HOLD_OUTPUT_PRESSED ON_HOLD_OSM
+#define TAPHOLD_OUTPUT_PRESSED ON_TAPHOLD_OSM
 
 
 #define STATE_PRESSED_MASK          constPow(2, STATE_PRESSED)
 #define STATE_DOUBLE_TAP_MASK       constPow(2, STATE_DOUBLE_TAP)
 #define STATE_PRESSANDRELEASE_MASK   constPow(2, STATE_PRESSANDRELEASE)
 #define STATE_TAPHOLD_MASK           constPow(2, STATE_TAPHOLD)
+#define OUTPUT_PRESSED_MASK          constPow(2, OUTPUT_PRESSED)
 #define HOLD_ENABLED_MASK            constPow(2, HOLD_ENABLED)
 #define DOUBLE_TAP_ENABLED_MASK      constPow(2, DOUBLE_TAP_ENABLED)
 #define TAPHOLD_ENABLED_MASK         constPow(2, TAPHOLD_ENABLED)
@@ -38,8 +42,17 @@
 #define ON_DOUBLETAP_OSM_MASK      constPow(2, ON_DOUBLETAP_OSM)
 #define ON_TAPHOLD_OSM_MASK        constPow(2, ON_TAPHOLD_OSM)
 #define UNCONSUMABLE_MASK         constPow(2, UNCONSUMABLE)
+#define HOLD_OUTPUT_PRESSED_MASK  constPow(2, HOLD_OUTPUT_PRESSED)
+#define TAPHOLD_OUTPUT_PRESSED_MASK constPow(2, TAPHOLD_OUTPUT_PRESSED)
 #define KEY_STATE STATE_PRESSED_MASK + STATE_DOUBLE_TAP_MASK \
 + STATE_TAPHOLD_MASK + STATE_PRESSANDRELEASE_MASK
+
+enum output_type {
+    tapT,
+    doubletapT,
+    holdT,
+    tapholdT
+};
 
 class mapping
 {
@@ -51,10 +64,10 @@ private:
     outputSeq  doubletap;
     outputSeq taphold;
 
-    //output press action
-    inline void output_press(const outputSeq &seq);
-    //output release action
     inline void output_release(const outputSeq &seq);
+    //write output event
+    template<output_type type>
+    inline void write_output();
 public:
     inline void output_event();
     inline void consume();
@@ -63,4 +76,5 @@ public:
     //key index is the index in array
     //constructor for key mapping
     mapping(TypeOutputConf key, outputSeq hold, outputSeq tap, outputSeq doubletap, outputSeq taphold);
+    mapping();
 };
