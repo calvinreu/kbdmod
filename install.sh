9859db9ec88ed01ff85d27fa371390b38184f4ed
calvinreu <j.calvin.reu@protonmail.com>
2022-07-11 18:29:35 +0200

    add building

--- install.sh
+++ install.sh
@@ -16,6 +16,12 @@ if [ -z "$DESTDIR" ]; then
 	DESTDIR="usr/local/bin"
 fi
 
+# build
+mkdir -p build
+cd build
+cmake ..
+make -j 8
+
 mkdir -p $DESTDIR
 cp -f build/keyboard_modify.bin $DESTDIR/keyboard_modify
 chmod 755 $DESTDIR/keyboard_modify
