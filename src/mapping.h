49b4180f4cb3bb2c7e4999613eb5a3e803d3a660
calvinreu <j.calvin.reu@protonmail.com>
2022-08-23 15:09:42 +0200

    .

--- src/mapping.h
+++ src/mapping.h
@@ -7,33 +7,8 @@
 #include <stdlib.h>
 #include <string>
 #include <vector>
+#include "mapping_def.h"
 
-
-#define OUTPUT_PRESSED 2
-#define ON_TAP_OSM 3
-#define ON_HOLD_OSM 4
-#define ON_DOUBLETAP_OSM 5
-#define ON_TAPHOLD_OSM 6
-#define DOUBLETAP_ENABLED 7
-#define TAPHOLD_ENABLED 8
-#define HOLD_ENABLED 9
-
-#define OUTPUT_PRESSED_MASK  	        constPow(2, OUTPUT_PRESSED   )
-#define ON_TAP_OSM_MASK  				constPow(2, ON_TAP_OSM       )
-#define ON_HOLD_OSM_MASK  				constPow(2, ON_HOLD_OSM      )
-#define ON_DOUBLETAP_OSM_MASK  			constPow(2, ON_DOUBLETAP_OSM )
-#define ON_TAPHOLD_OSM_MASK  			constPow(2, ON_TAPHOLD_OSM   )
-#define DOUBLETAP_ENABLED_MASK  		constPow(2, DOUBLETAP_ENABLED)
-#define TAPHOLD_ENABLED_MASK  			constPow(2, TAPHOLD_ENABLED  )
-#define HOLD_ENABLED_MASK  				constPow(2, HOLD_ENABLED     )
-#define KEY_STATE 						7
-#define TAP_OUTPUT_PRESSED_MASK  		0 + OUTPUT_PRESSED_MASK
-#define HOLD_OUTPUT_PRESSED_MASK  		1 + OUTPUT_PRESSED_MASK
-#define DOUBLETAP_OUTPUT_PRESSED_MASK  	2 + OUTPUT_PRESSED_MASK
-#define TAPHOLD_OUTPUT_PRESSED_MASK  	3 + OUTPUT_PRESSED_MASK
-#define SINGLE_PRESS_MASK  				1
-#define SINGLE_RELEASE_MASK  			2
-#define DOUBLE_PRESS_MASK  				3
 
 enum output_type {
     tapT,
@@ -62,7 +37,7 @@ private:
 public:
 	inline const OutputStorage& get_output() const { return output; }
     inline bool passthrough() const { return output.is_empty(); }
-	inline bool noqueue() const { return key < ON_HOLD_OSM_MASK; }
+	inline bool notapdance() const { return key < ON_HOLD_OSM_MASK; }
 	inline bool tap_osm() const { return key == ON_TAP_OSM_MASK; }
 	void timeout_event();
     void release();
