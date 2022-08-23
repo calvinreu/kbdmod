94e3f3e3cd9a0c13834b6663086f11e65ff40e28
calvinreu <j.calvin.reu@protonmail.com>
2022-08-23 16:52:10 +0200

    .

--- src/autoshift.h
+++ src/autoshift.h
@@ -1,42 +1,8 @@
 #pragma once
 #include "mapping.h"
+#include "mapping_def.h"
 
-extern std::vector<mapping> keyMapBase;
-extern uint8_t autoShift;
-
-inline void autoshift_init(const KeyCode &code, TypeOutputConf *outputConf) {
-	if (*outputConf & HOLD_ENABLED_MASK)
-		return;
-
-	if (autoShift & AUTOSHIFT_DIGIT_MASK && code >= KEY_1 && code <= KEY_0)
-		*outputConf |= AUTOSHIFT_CAPABLE_MASK;
-
-	if (autoShift & AUTOSHIFT_LETTER_MASK &&
-		((code >= KEY_Q && code <= KEY_P) || (code >= KEY_A && code <= KEY_L) || (code >= KEY_Z && code <= KEY_M)))
-		*outputConf |= AUTOSHIFT_CAPABLE_MASK;
-
-	if (autoShift & AUTOSHIFT_SPECIAL_CHARACTER_MASK &&
-	(code == KEY_MINUS || code == KEY_EQUAL || code == KEY_LEFTBRACE || code == KEY_RIGHTBRACE || (code >= KEY_COMMA && code <= KEY_SLASH)))
-		*outputConf |= AUTOSHIFT_CAPABLE_MASK;
-}
-
-inline void enable_autoshift() {
-	for (auto &it : keyMapBase) {
-		if (it.key & AUTOSHIFT_CAPABLE_MASK)
-			it.key |= HOLD_ENABLED_MASK;
-	}
-}
-
-inline void disable_autoshift() {
-	for (auto &it : keyMapBase) {
-		if (it.key & AUTOSHIFT_CAPABLE_MASK)
-			it.key &= ~HOLD_ENABLED_MASK;
-	}
-}
-
-inline void toggle_autoshift() {
-	if (autoShift)
-		disable_autoshift();
-	else
-		enable_autoshift();
-}
+extern void autoshift_init(const KeyCode &code, TypeOutputConf *outputConf);
+extern void enable_autoshift();
+extern void disable_autoshift();
+extern void toggle_autoshift();
