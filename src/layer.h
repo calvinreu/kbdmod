336adee795f7cf9376af5aaabcd635c0d61f9c1d
calvinreu <j.calvin.reu@protonmail.com>
2022-08-23 23:13:40 +0200

    cpp can be a bitch

--- src/layer.h
+++ src/layer.h
@@ -1,21 +1,15 @@
 #pragma once
 #include "mapping.h"
-#include <yaml-cpp/yaml.h>
 
 struct Layer{
 	mapping* mappings;
 	uint8_t min;
 	uint8_t max;
-	inline Layer(uint min, uint max)
-	: min(min), max(max), mappings(new mapping[max - min + 1] - min){}
-	inline ~Layer(){
-		delete[] (mappings + min);
-	}
+	Layer(uint min, uint max);
+	~Layer();
 	inline Layer(){}
 	inline mapping* begin(){return mappings;}
-	inline mapping* end(){return mappings + (max - min + 1);}
-	inline mapping &operator[](uint8_t key){
-		return mappings[key];
-	}
+	mapping* end();
+	mapping &operator[](uint8_t key);
 	inline uint8_t size(){return max - min + 1;}
 };
